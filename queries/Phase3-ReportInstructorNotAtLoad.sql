043f6772481a1e56af56b4ee138d0d8b029a5c24
Ben Anderson <78427672+bennyy3@users.noreply.github.com>
2022-12-09 14:12:07 -0600

    Add Report Queries Phase 3

--- queries/Phase3-ReportInstructorNotAtLoad.sql
+++ queries/Phase3-ReportInstructorNotAtLoad.sql
@@ -1,7 +1,11 @@
-with instructorTeaches(instructor_id, section_id)
-as (select instructor_id, section_id from Instructor left outer join Teaches using(instructor_id))
-select instructor_id, teu
+with instructorTeaches(instructor_id, section_id, desired_load)
+as (select instructor_id, section_id, desired_load from Instructor left outer join Teaches using(instructor_id)),
+instructorTEU(instructor_id, teu, desired_load)
+as (select instructor_id, teu, desired_load
 from instructorTeaches
 left outer join Section using(section_id)
 left outer join Course using(course_id)
-left outer join TEU using(num_credits);
+left outer join TEU using(num_credits)),
+instructorTEUsum(instructor_id, teuSum, desired_load) as
+(select instructor_id, SUM(teu) as teuSum, desired_load from instructorTEU group by instructor_id)
+select * from instructorTEUsum where teuSum < (desired_load + .3) OR teuSum > (desired_load + .3) or teuSum is NULL;
